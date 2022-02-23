3029a5fa273c372606cf99e000fa7c8fa621a163
Abdul Maajid <amaajid0@gmail.com>
2022-02-24 00:51:27 +0500

    done changes on hero

--- assets/YouTube Assets-20220220T194201Z-001/YouTube Assets/schema.js
+++ assets/YouTube Assets-20220220T194201Z-001/YouTube Assets/schema.js
@@ -1,14 +1,5 @@
-// First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
-
-// Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
-
-export default createSchema({
-  // We name our schema
+const schema = {
   name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     {
       name: 'users',
@@ -102,4 +93,4 @@ export default createSchema({
       ],
     },
   ]),
-})
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
 import type { AppProps } from 'next/app'
+
+const supportedChains = [4];
+const connectors = {
+  injected: {},
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
